a313f1115d7eb4a80b30f344e28b3f0abb9d04a3
Sushant2 <er.sushant.2@gmail.com>
2022-01-21 19:17:04 +0530

    addLast() implemented

--- Linked List/LL.java
+++ Linked List/LL.java
@@ -3,7 +3,27 @@ import java.util.*;
 
 public class LL {
     public static class Node {
+        int data;
+        Node next;
+    }
 
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size; // size of linked list
+
+        public void addLast(int val) {
+            Node temp = new Node();
+            temp.data = val;
+            if (size == 0) {
+                head = temp;
+                tail = temp;
+            } else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
     }
 
     public static void testList(LinkedList list) {
