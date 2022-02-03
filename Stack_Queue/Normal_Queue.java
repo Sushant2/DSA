99913b47d391b718ca9429739b716b71cc53c95d
Sushant2 <er.sushant.2@gmail.com>
2022-02-03 19:57:00 +0530

    normal queue using java collections

---
+++ Stack_Queue/Normal_Queue.java
@@ -0,0 +1,39 @@
+import java.io.*;
+import java.util.*;
+
+public class Normal_Queue {
+    public static void main(String[] args) throws Exception {
+        // ! we don't declare/initialise queue like this cos, queue is an interface &
+        // interfaces can't be instantiated
+        // Queue<Integer> q = new Queue<>();
+        // ? so, we declare it like this
+        Queue<Integer> q = new ArrayDeque<>();
+        q.add(10);
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        q.add(20);
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        q.add(30);
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        q.add(40);
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        System.out.println("Curr Size: " + q.size());
+        q.remove();
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        q.remove();
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        q.remove();
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        q.remove();
+        System.out.println("Curr Size: " + q.size());
+        System.out.println("Curr Queue: " + q);
+        System.out.println("Curr Top: " + q.peek());
+        q.remove();
+    }
+}
