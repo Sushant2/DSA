f3fc2db66a018381e6305ecf81f41ea92c2defe3
Sushant2 <er.sushant.2@gmail.com>
2022-02-06 03:21:10 +0530

    queue using stack,peek/remove efficient

---
+++ Stack_Queue/Stack_Queue_Adapter2.java
@@ -0,0 +1,73 @@
+//! Remove/Peek Efficient - O(1)
+//! Add - O(n)
+
+import java.io.*;
+import java.util.*;
+
+public class Stack_Queue_Adapter2 {
+    public static class queueUsingStack {
+        Stack<Integer> mainS;
+        Stack<Integer> helperS;
+
+        public queueUsingStack() {
+            mainS = new Stack<>();
+            helperS = new Stack<>();
+        }
+
+        int size() {
+            return mainS.size();
+        }
+
+        void add(int val) {
+            while (mainS.size() > 0) {
+                helperS.push(mainS.pop());
+            }
+            mainS.push(val);
+            while (helperS.size() > 0) {
+                mainS.push(helperS.pop());
+            }
+        }
+
+        int remove() {
+            if (size() == 0) {
+                System.out.println("Queue underflow!");
+                return -1;
+            }
+            return mainS.pop();
+        }
+
+        int peek() {
+            if (size() == 0) {
+                System.out.println("Queue underflow!");
+                return -1;
+            }
+            return mainS.peek();
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        queueUsingStack qu = new queueUsingStack();
+
+        String str = br.readLine();
+        while (str.equals("quit") == false) {
+            if (str.startsWith("add")) {
+                int val = Integer.parseInt(str.split(" ")[1]);
+                qu.add(val);
+            } else if (str.startsWith("remove")) {
+                int val = qu.remove();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("peek")) {
+                int val = qu.peek();
+                if (val != -1) {
+                    System.out.println(val);
+                }
+            } else if (str.startsWith("size")) {
+                System.out.println(qu.size());
+            }
+            str = br.readLine();
+        }
+    }
+}
