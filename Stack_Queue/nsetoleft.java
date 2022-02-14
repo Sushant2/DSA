7d0d2aa6f335bfb84cd3987dd1768a8c7ef3e6a1
Sushant2 <er.sushant.2@gmail.com>
2022-02-14 15:42:14 +0530

    next smaller to ele using left to right approach

---
+++ Stack_Queue/nsetoleft.java
@@ -0,0 +1,45 @@
+import java.io.*;
+import java.util.*;
+
+public class nsetoleft {
+    // ! approach - O(n)
+    // ? traverse from left to right
+    // ? pop those who are greater or equal to arr[i]
+    // ? stk.peek() will be ur answer after popping
+    // ? push urself cos u can be answer of someone
+    public static int[] nsetoleft1(int[] arr) {
+        int[] nsel = new int[arr.length];
+        Arrays.fill(nsel, -1);
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (stk.size() > 0 && arr[stk.peek()] >= arr[i]) {
+                stk.pop();
+            }
+            if (stk.size() > 0) {
+                nsel[i] = stk.peek();
+            }
+            stk.push(i);
+        }
+        return nsel;
+    }
+
+    public static void display(int[] arr) {
+        StringBuilder str = new StringBuilder();
+        for (int val : arr) {
+            str.append(val + " ");
+        }
+        System.out.println(str);
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] a = new int[n];
+        for (int i = 0; i < n; i++) {
+            a[i] = Integer.parseInt(br.readLine());
+        }
+        int[] nsel = nsetoleft1(a);
+        display(nsel);
+
+    }
+}
