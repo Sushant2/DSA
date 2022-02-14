a44f751f6476325686c99a6041241d063c936583
Sushant2 <er.sushant.2@gmail.com>
2022-02-14 17:32:42 +0530

    max area of histogram using nsel, nser

---
+++ Stack_Queue/maxAreaHistogram.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class maxAreaHistogram {
+    public static int[] nseright(int[] arr) {
+        int[] nser = new int[arr.length];
+        Arrays.fill(nser, arr.length);
+        Stack<Integer> stk = new Stack<>();
+        for (int i = arr.length - 1; i >= 0; i--) {
+            while (stk.size() > 0 && arr[stk.peek()] >= arr[i]) {
+                stk.pop();
+            }
+            if (stk.size() > 0) {
+                nser[i] = stk.peek();
+            }
+            stk.push(i);
+        }
+        return nser;
+    }
+
+    public static int[] nseleft(int[] arr) {
+        int[] nsel = new int[arr.length];
+        Arrays.fill(nsel, -1);
+        Stack<Integer> stk = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            while (stk.size() > 0 && arr[stk.peek()] >= arr[i]) {
+                stk.pop();
+            }
+            if (stk.size() > 0) {
+                nsel[i] = stk.peek();
+            }
+            stk.push(i);
+        }
+        return nsel;
+    }
+
+    public static void main(String[] str) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+        int[] nsel = nseleft(arr);
+        int[] nser = nseright(arr);
+        int maxArea = 0;
+        for (int i = 0; i < n; i++) {
+            int currArea = (nser[i] - nsel[i] - 1) * arr[i];
+            maxArea = Math.max(maxArea, currArea);
+        }
+        System.out.println("Max Area Of Histogram: " + maxArea);
+    }
+}
