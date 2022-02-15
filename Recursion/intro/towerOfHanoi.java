98a3741c4c24f7f4c7ce79b2cf1adc28f0656f0a
Sushant2 <er.sushant.2@gmail.com>
2022-02-16 03:07:48 +0530

    done tower of hanoi

--- Recursion/intro/towerOfHanoi.java
+++ Recursion/intro/towerOfHanoi.java
@@ -1,3 +1,26 @@
+import java.io.*;
+import java.util.*;
+
 public class towerOfHanoi {
-    
+    public static void toh(int n, int src, int des, int aux) {
+        // base case
+        if (n == 0)
+            return;
+        // faith
+        toh(n - 1, src, aux, des);
+        // meeting expec
+        System.out.println(n + " [ " + src + " -> " + des + " ]");
+        // faith
+        toh(n - 1, aux, des, src);
+    }
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int A = scn.nextInt();
+        int B = scn.nextInt();
+        int C = scn.nextInt();
+        toh(n, A, B, C);
+        scn.close();
+    }
 }
