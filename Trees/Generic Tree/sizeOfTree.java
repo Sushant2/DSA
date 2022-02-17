8213432444d8fa98cbe25fe2b9926f1b1ff1d75f
Sushant2 <er.sushant.2@gmail.com>
2022-02-17 18:47:37 +0530

    size of a generic tree

---
+++ Trees/Generic Tree/sizeOfTree.java
@@ -0,0 +1,49 @@
+import java.io.*;
+import java.nio.Buffer;
+import java.util.*;
+
+public class sizeOfTree {
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static Node construct(int[] arr) {
+        Node root = null;
+        Stack<Node> stk = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1) {
+                stk.pop();
+            } else {
+                Node curr = new Node();
+                curr.data = arr[i];
+                if (stk.size() == 0)
+                    root = curr;
+                else
+                    stk.peek().children.add(curr);
+                stk.push(curr);
+            }
+        }
+        return root;
+    }
+
+    public static int size(Node node) {
+        int count = 1;
+        for (Node child : node.children) {
+            count += size(child);
+        }
+        return count;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(values[i]);
+        }
+        Node root = construct(arr);
+        System.out.println(size(root));
+    }
+}
