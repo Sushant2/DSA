fd04fbd2ee3fbb10ff849600d553226076b1cce6
Sushant2 <er.sushant.2@gmail.com>
2022-02-21 20:12:07 +0530

    height of tree on the basis of node

---
+++ Trees/Generic Tree/heightOfTree.java
@@ -0,0 +1,69 @@
+import java.io.*;
+import java.util.*;
+
+public class heightOfTree {
+    private static class Node {
+        int data;
+        ArrayList<Node> children = new ArrayList<>();
+    }
+
+    public static Node construct(int[] arr) {
+        Node root = null;
+        Stack<Node> stk = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == -1)
+                stk.pop();
+            else {
+                Node curr = new Node();
+                curr.data = arr[i];
+                if (stk.size() == 0)
+                    root = curr;
+                else
+                    stk.peek().children.add(curr);
+                stk.push(curr);
+            }
+        }
+        return root;
+    }
+
+    public static void display(Node node) {
+        // corner case
+        if (node == null)
+            return;
+        String str = node.data + " -> ";
+        for (Node child : node.children) {
+            str += child.data + ", ";
+        }
+        str += ".";
+        System.out.println(str);
+        // faith
+        for (Node child : node.children) {
+            display(child);
+        }
+    }
+
+    public static int heightTree(Node node) {
+        // edgecase
+        if (node == null)
+            return 0;
+        int height = 0;
+        for (Node child : node.children) {
+            height = Math.max(height, heightTree(child));
+        }
+        return 1 + height;
+    }
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(values[i]);
+        }
+        Node root = construct(arr);
+        display(root);
+        System.out.println("Height of tree: " + heightTree(root));
+    }
+}
