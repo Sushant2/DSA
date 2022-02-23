1ea700ac2316d2b456df4d309a25f75d5f27b6d1
Sushant2 <er.sushant.2@gmail.com>
2022-02-24 02:10:40 +0530

    remove duplicates 1

---
+++ Linked List/removeDuplicates1.java
@@ -0,0 +1,70 @@
+import java.util.*;
+import java.io.*;
+
+public class removeDuplicates1 {
+
+    public static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static class LinkedList {
+        Node head;
+        Node tail;
+        int size;
+
+        public int size() {
+            return size;
+        }
+
+        public void addLast(int val) {
+            Node temp = new Node(val);
+            if (size == 0)
+                head = tail = temp;
+            else {
+                tail.next = temp;
+                tail = temp;
+            }
+            size++;
+        }
+    }
+
+    public static void display(Node head) {
+        while (head != null) {
+            System.out.print(head.data + " ");
+            head = head.next;
+        }
+        System.out.println();
+    }
+
+    // ! In O(n) - time complexity
+    public static void removeDuplicates(Node head) {
+        Node dummy = new Node(-1);
+        Node tail = dummy;
+        while (head != null) {
+            if (tail == dummy || head.data != tail.data) {
+                tail.next = head;
+                tail = head;
+            }
+            head = head.next;
+        }
+        tail.next = null;
+        head = dummy.next;
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        LinkedList list = new LinkedList();
+        int n = Integer.parseInt(br.readLine());
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            list.addLast(Integer.parseInt(values[i]));
+        }
+        removeDuplicates(list.head);
+        display(list.head);
+    }
+}
