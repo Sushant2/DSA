ceda5bd4cdad193cfd8cd0bf9023e80796c421c9
Sushant2 <er.sushant.2@gmail.com>
2022-02-25 23:59:47 +0530

    added perform operation fucntion

--- Stack_Queue/infixEvaluation.java
+++ Stack_Queue/infixEvaluation.java
@@ -11,6 +11,17 @@ public class infixEvaluation {
         return 0;
     }
 
+    public static int performOp(int a, char op, int b) {
+        if (op == '+')
+            return a + b;
+        else if (op == '-')
+            return a - b;
+        else if (op == '*')
+            return a * b;
+        else
+            return a / b;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String exp = br.readLine();
