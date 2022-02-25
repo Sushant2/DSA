3520f5a0a3213f8791b192b09ec3016d4b7d4176
Sushant2 <er.sushant.2@gmail.com>
2022-02-25 23:57:20 +0530

    added precedence function for infix evaluation

--- Stack_Queue/infixEvaluation.java
+++ Stack_Queue/infixEvaluation.java
@@ -2,6 +2,15 @@ import java.io.*;
 import java.util.*;
 
 public class infixEvaluation {
+
+    public static int precedence(char ch) {
+        if (ch == '+' || ch == '-')
+            return 2;
+        if (ch == '*' || ch == '/')
+            return 1;
+        return 0;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String exp = br.readLine();
