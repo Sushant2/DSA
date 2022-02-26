b02b5ee3062f90aea244fd004e2c3e55f71ecf93
Sushant2 <er.sushant.2@gmail.com>
2022-02-27 02:56:52 +0530

    initial commits in basic calculator

---
+++ Stack_Queue/basicCal.java
@@ -0,0 +1,15 @@
+import java.io.*;
+import java.util.*;
+
+//! this basic calculator will work for LC 224, LC 227 & lintcode 849!
+//! Handles all edge/cases with some manipulation in the input string - by refining it through our function
+//? which handles spaces, unary operators n all
+//! our code will also handle digits more than 1 e.g (223+3122)*12-4
+//! but, except for power precedence we can do that also
+
+public class basicCal {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String exp = br.readLine();
+    }
+}
