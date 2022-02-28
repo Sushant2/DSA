4928703664238b6833d47e5dc9ec0568246e6364
Sushant2 <er.sushant.2@gmail.com>
2022-02-28 23:46:58 +0530

    celebrity problem

--- Stack_Queue/celebrityProblem.java
+++ Stack_Queue/celebrityProblem.java
@@ -1,3 +1,9 @@
+import java.util.*;
+import java.io.*;
+
 public class celebrityProblem {
-    
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    }
 }
