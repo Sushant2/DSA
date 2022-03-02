efd7902ef1d2dd05ae95704d3442ea7d33903666
Sushant2 <er.sushant.2@gmail.com>
2022-03-02 21:00:58 +0530

    smallest number following patter

--- Stack_Queue/smallestNumFollowPattern.java
+++ Stack_Queue/smallestNumFollowPattern.java
@@ -6,7 +6,24 @@ public class smallestNumFollowPattern {
     public static void smallestNumberFollowingPattern(String str) {
         Stack<Integer> stk = new Stack<>();
         int num = 1;
-        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+            if (ch == 'd') {
+                stk.push(num);
+                num++;
+            } else {
+                stk.push(num);
+                num++;
+                while (stk.size() != 0) {
+                    System.out.print(stk.pop());
+                }
+            }
+        }
+        // at last pust remaining
+        stk.push(num);
+        while (stk.size() > 0) {
+            System.out.print(stk.pop());
+        }
     }
 
     public static void main(String[] args) throws Exception {
