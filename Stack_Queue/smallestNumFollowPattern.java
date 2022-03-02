0c75286deaa265747aa6aa88d908c93beb89c712
Sushant2 <er.sushant.2@gmail.com>
2022-03-02 20:57:02 +0530

    smallest number following pattern initial

---
+++ Stack_Queue/smallestNumFollowPattern.java
@@ -0,0 +1,17 @@
+import java.util.*;
+import java.io.*;
+
+public class smallestNumFollowPattern {
+
+    public static void smallestNumberFollowingPattern(String str) {
+        Stack<Integer> stk = new Stack<>();
+        int num = 1;
+        
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        smallestNumberFollowingPattern(str);
+    }
+}
