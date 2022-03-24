21e4130a2ce5adb2da3d8e683a019b1455cb6922
Sushant2 <er.sushant.2@gmail.com>
2022-03-24 18:27:37 +0530

    node with maximum sum in generic tree using class/object

--- Trees/Generic Tree/TCNodewithMaxSum.java
+++ Trees/Generic Tree/TCNodewithMaxSum.java
@@ -53,6 +53,34 @@ public class TCNodewithMaxSum {
         return sum;
     }
 
+    // w/o using static vars - using class pair object
+    public static class Pair {
+        int maxSum;
+        Node maxSumNode;
+        int sum;
+
+        Pair(int sum) {
+            this.sum = sum;
+        }
+    }
+
+    public static Pair subTreeSum(Node root) {
+        Pair ans = new Pair(root.data);
+        for (Node child : root.children) {
+            Pair temp = subTreeSum(child);
+            ans.sum += temp.sum;
+            if (temp.maxSum > ans.maxSum) {
+                ans.maxSum = temp.maxSum;
+                ans.maxSumNode = temp.maxSumNode;
+            }
+        }
+        if (ans.sum > ans.maxSum) {
+            ans.maxSum = ans.sum;
+            ans.maxSumNode = root;
+        }
+        return ans;
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -63,7 +91,8 @@ public class TCNodewithMaxSum {
         }
 
         Node root = constructTree(arr);
-        nodeWithMaxSum(root);
-        System.out.println("Node with maximum sum: " + maxSumNode.data + "@" + maxSum);
+        // nodeWithMaxSum(root);
+        Pair ans = subTreeSum(root);
+        System.out.println("Node with maximum sum: " + ans.maxSumNode.data + "@" + ans.maxSum);
     }
 }
