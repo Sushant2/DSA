bea0b2c3809c541dcb036f4810372c39e306e1de
Sushant2 <er.sushant.2@gmail.com>
2022-03-26 12:26:37 +0530

    recursively pre,in,post order traversal of binary tree

--- Trees/Binary Tree/recursivelyPreInPostTraversal.java
+++ Trees/Binary Tree/recursivelyPreInPostTraversal.java
@@ -78,6 +78,42 @@ public class recursivelyPreInPostTraversal {
         // postorder
     }
 
+    // ! preorder, post order, inorder
+
+    public static void preorder(Node root) {
+        if (root == null)
+            return;
+
+        System.out.print(root.data + " ");
+
+        preorder(root.left);
+        preorder(root.right);
+    }
+
+    public static void inorder(Node root) {
+        if (root == null)
+            return;
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public static void postorder(Node root) {
+        if (root == null)
+            return;
+        postorder(root.left);
+        postorder(root.right);
+        System.out.print(root.data + " ");
+    }
+
+    public static void recPreInPost(Node root) {
+        preorder(root);
+        System.out.println();
+        inorder(root);
+        System.out.println();
+        postorder(root);
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -91,5 +127,6 @@ public class recursivelyPreInPostTraversal {
         }
         Node root = constructTree(arr);
         displayTree(root);
+        recPreInPost(root);
     }
 }
