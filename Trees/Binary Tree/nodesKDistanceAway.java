cef17a880d539f15e77ac397ce9c4e9d29d5c319
Sushant2 <er.sushant.2@gmail.com>
2022-03-28 22:47:20 +0530

    get node to root path & print k nodes down

--- Trees/Binary Tree/nodesKDistanceAway.java
+++ Trees/Binary Tree/nodesKDistanceAway.java
@@ -53,6 +53,43 @@ public class nodesKDistanceAway {
         return root;
     }
 
+    public static ArrayList<Node> nodeToRoot(Node root, int data) {
+        // -ve base case
+        if (root == null)
+            return new ArrayList<>();
+        // +ve base case
+        if (root.data == data) {
+            ArrayList<Node> base = new ArrayList<>();
+            base.add(root);
+            return base;
+        }
+        ArrayList<Node> lans = nodeToRoot(root.left, data);
+        if (lans.size() > 0) {
+            lans.add(root);
+            return lans;
+        }
+        ArrayList<Node> rans = nodeToRoot(root.right, data);
+        if (rans.size() > 0) {
+            rans.add(root);
+            return rans;
+        }
+        return new ArrayList<>();
+    }
+
+    public static void printKFarAway(Node root, int data, int k) {
+        if (root == null)
+            return;
+        // get node to root path in arrayList
+        ArrayList<Node> n2r = nodeToRoot(root, data);
+        for (int i = 0; i < n2r.size(); i++) {
+            if (k >= 0) {
+                Node blockage = (i == 0) ? null : n2r.get(i - 1);
+                printKDowns(n2r.get(i), k);
+                k--;
+            }
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
