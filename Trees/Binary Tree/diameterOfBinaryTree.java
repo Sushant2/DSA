76452baf6b960dc2e227e17c25a3ee1c6207027b
Sushant2 <er.sushant.2@gmail.com>
2022-03-31 23:59:18 +0530

    diameter of binary tree using brute force/returning diameter

---
+++ Trees/Binary Tree/diameterOfBinaryTree.java
@@ -0,0 +1,133 @@
+import java.io.*;
+import java.util.*;
+
+//TODO - IMP que : diameter of binary tree/generic tree n rest
+//! DIAMETER Definition - 
+//? maximum distance between 2 nodes/leafnodes(but almost in every case nodes are leaf nodes){in terms of edges}
+//? it's not necessary that diameter passes through root node
+public class diameterOfBinaryTree {
+    private static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static Node constructBTree(Integer[] arr) {
+        Node root = new Node(arr[0]);
+        Stack<Pair> stk = new Stack<>();
+        stk.push(new Pair(root, -1));
+        int idx = 0;
+        while (!stk.isEmpty()) {
+            Pair par = stk.peek();
+            if (par.state == -1) {
+                // preorder
+                idx++;
+                if (arr[idx] != null) {
+                    Node child = new Node(arr[idx]);
+                    par.node.left = child;
+                    stk.push(new Pair(child, -1));
+                }
+                par.state++;
+            } else if (par.state == 0) {
+                // inorder
+                idx++;
+                if (arr[idx] != null) {
+                    Node child = new Node(arr[idx]);
+                    par.node.right = child;
+                    stk.push(new Pair(child, -1));
+                }
+                par.state++;
+            } else
+                stk.pop();
+        }
+        return root;
+    }
+
+    public static void displayBTree(Node root) {
+        if (root == null)
+            return;
+        if (root.left != null)
+            System.out.print(root.left.data);
+        else
+            System.out.print(".");
+        System.out.print(" <- " + root.data + " -> ");
+        if (root.right != null)
+            System.out.print(root.right.data);
+        else
+            System.out.print(".");
+
+        System.out.println();
+        // preorder
+        displayBTree(root.left);
+        // inorder
+        displayBTree(root.right);
+        // postorder
+    }
+
+    // leaf node of maximum depth in left subtree
+    // leaf node of maximum depth in right subtree
+    // (dono maximum depths leafs ki, ek hi subtreem ho skti h)
+    // distance b/w them is the diameter of Btree
+
+    // ! BRUTE FORCE - 1stapproach - O(n^2)
+
+    // height in terms of edge
+    public static int height(Node node) {
+        if (node == null)
+            return -1;
+        int lh = height(node.left);
+        int rh = height(node.right);
+        int th = Math.max(lh, rh) + 1;
+        return th;
+    }
+
+    // Apply travel and change strategy -
+    // 1. node par khade hokar node ka diameter nikalenge that is - leftheight +
+    // rightheight + 2
+    // 2. & sare diameter mein se jo sbse maxm diameter milega wahi poora tree ka
+    // diameter h
+    public static int diameter(Node root) {
+        if (root == null)
+            return 0;
+        int lDia = diameter(root.left);
+        int rDia = diameter(root.right);
+
+        // meeting expec
+        int lHeight = height(root.left);
+        int rHeight = height(root.right);
+
+        int dia = lHeight + rHeight + 2;
+
+        return Math.max(dia, Math.max(lDia, rDia));
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        // !we're creating array using non primitive data type/class cos, "Integer"
+        // class can also store/hold null values
+        Integer[] arr = new Integer[n];
+        String[] values = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            if (values[i].equals("n") == false)
+                arr[i] = Integer.parseInt(values[i]);
+            else
+                arr[i] = null;
+        }
+        Node root = constructBTree(arr);
+    }
+}
