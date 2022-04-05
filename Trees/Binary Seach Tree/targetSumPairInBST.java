0078f8b8f7baabb04982a6b6f6dcbbc7b83640ae
Sushant2 <er.sushant.2@gmail.com>
2022-04-05 20:09:52 +0530

    initials

---
+++ Trees/Binary Seach Tree/targetSumPairInBST.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class targetSumPairInBST {
+
+    private static class Node {
+        int data;
+        Node left;
+        Node right;
+
+        Node(int data) {
+            this.data = data;
+        }
+    }
+
+    public static Node construct(int[] arr, int low, int high) {
+        // base case
+        if (low > high)
+            return null;
+        int mid = (low + high) / 2;
+        Node root = new Node(arr[mid]);
+        root.left = construct(arr, low, mid - 1);
+        root.right = construct(arr, mid + 1, high);
+        return root;
+    }
+
+    public static void display(Node root) {
+        if (root == null)
+            return;
+        display(root.left);
+        System.out.print(root.data + " ");
+        display(root.right);
+    }
+
+    
+
+    public static void main(String[] args) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] arr = new int[n];
+        String values[] = br.readLine().split(" ");
+        for (int i = 0; i < n; i++) {
+            arr[i] = Integer.parseInt(values[i]);
+        }
+        Node root = construct(arr, 0, n - 1);
+        int targetSum = Integer.parseInt(br.readLine()));
+        display(root);
+    }
+}
