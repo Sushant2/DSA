1a45c38da894c2e1f1736945d587f55fa35030f8
Sushant2 <er.sushant.2@gmail.com>
2022-04-05 22:46:18 +0530

    target sum pair in BST 3rd/efficient approach - inorder, reverse inorder + 2 pointers

--- Trees/Binary Seach Tree/targetSumPairInBST.java
+++ Trees/Binary Seach Tree/targetSumPairInBST.java
@@ -47,8 +47,24 @@ public class targetSumPairInBST {
             return true;
     }
 
+    public static void targetSum(Node curr, int sum, Node root) {
+        if (curr == null)
+            return;
+        targetSum(curr.left, sum, root);
+        // inorder
+        // stop when data > sum/2
+        if (curr.data >= (sum / 2))
+            return;
+        int data = sum - curr.data;
+        // ! NOTICE :we're passing root to find, not curr node
+        if (find(root, data))
+            System.out.println(curr.data + " " + data);
+        targetSum(curr.right, sum, root);
+    }
+
     // ! 2nd approach - Easiest apprach - Store nodes in array & use 2 pointers from
     // ! start & end of array
+    // time comp - O(n) & space comp - o(n)
     public static void inorder(Node root, ArrayList<Integer> arr) {
         if (root == null)
             return;
@@ -75,19 +91,84 @@ public class targetSumPairInBST {
         }
     }
 
-    public static void targetSum(Node curr, int sum, Node root) {
-        if (curr == null)
-            return;
-        targetSum(curr.left, sum, root);
-        // inorder
-        // stop when data > sum/2
-        if (curr.data >= (sum / 2))
-            return;
-        int data = sum - curr.data;
-        // ! NOTICE :we're passing root to find, not curr node
-        if (find(root, data))
-            System.out.println(curr.data + " " + data);
-        targetSum(curr.right, sum, root);
+    // ! 3rd approach - Effiecient Apprach - Inorder traversal + Reverse Inorder
+    // ! Traversal + 2 pointers
+    // we can't use recursion on inorder/reverse inorder cos, we can't hold nodes to
+    // calclute sum of nodes
+
+    // ? so we'll use iterative inorder + iterative reverse inorder
+    public static class Pair {
+        Node node;
+        int state;
+
+        Pair(Node node, int state) {
+            this.node = node;
+            this.state = state;
+        }
+    }
+
+    public static void targetSum3(Node root, int target) {
+        Stack<Pair> inorder = new Stack<>();
+        inorder.push(new Pair(root, -1));
+        Stack<Pair> reverseInorder = new Stack<>();
+        reverseInorder.push(new Pair(root, -1));
+
+        int left = iterativeInorder(inorder);
+        int right = iterativeReverseInorder(reverseInorder);
+        while (left < right) {
+            if (left + right == target) {
+                System.out.println(left + " " + right);
+                left = iterativeInorder(inorder);
+                right = iterativeReverseInorder(reverseInorder);
+            } else if (left + right < target)
+                left = iterativeInorder(inorder);
+            else
+                right = iterativeReverseInorder(reverseInorder);
+        }
+    }
+
+    public static int iterativeInorder(Stack<Pair> inorder) {
+        while (!inorder.isEmpty()) {
+            Pair par = inorder.peek();
+            if (par.state == -1) {
+                // preorder
+                if (par.node.left != null)
+                    inorder.push(new Pair(par.node.left, -1));
+                par.state++;
+            } else if (par.state == 0) {
+                // inorder
+                int val = par.node.data;
+                if (par.node.right != null)
+                    inorder.push(new Pair(par.node.right, -1));
+                par.state++;
+                return val;
+            } else if (par.state == 1)
+                // postorder
+                inorder.pop();
+        }
+        return -1;
+    }
+
+    public static int iterativeReverseInorder(Stack<Pair> reverseInorder) {
+        while (!reverseInorder.isEmpty()) {
+            Pair par = reverseInorder.peek();
+            if (par.state == -1) {
+                // reverse preorder
+                if (par.node.right != null)
+                    reverseInorder.push(new Pair(par.node.right, -1));
+                par.state++;
+            } else if (par.state == 0) {
+                // reverse inorder
+                int val = par.node.data;
+                if (par.node.left != null)
+                    reverseInorder.push(new Pair(par.node.left, -1));
+                par.state++;
+                return val;
+            } else if (par.state == 1)
+                // reverse postorder
+                reverseInorder.pop();
+        }
+        return -1;
     }
 
     public static void main(String[] args) throws Exception {
@@ -101,9 +182,10 @@ public class targetSumPairInBST {
         }
         Node root = construct(arr, 0, n - 1);
         int target = Integer.parseInt(br.readLine());
-        display(root);
+        // display(root);
         // targetSum(root, target, root);
-        targetSum2(root, target);
+        // targetSum2(root, target);
+        targetSum3(root, target);
 
     }
 }
