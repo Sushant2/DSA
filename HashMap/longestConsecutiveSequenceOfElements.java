e48b4b00a9ef49bf94f3f77db98ac0869510ffd4
Sushant2 <er.sushant.2@gmail.com>
2022-04-08 02:00:03 +0530

    longest consecutive sequence of elements optimised in O(n)

--- HashMap/longestConsecutiveSequenceOfElements.java
+++ HashMap/longestConsecutiveSequenceOfElements.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class longestConsecutiveSequenceOfElements {
 
+    // ! BRUTE FORCE
     // sorting & traverse - O(nlogn + n)
     public static void longestConsecutiveElementsBrute(int[] arr) {
         if (arr.length == 0)
@@ -33,6 +34,37 @@ public class longestConsecutiveSequenceOfElements {
         }
     }
 
+    // ! Using HashMap - time comp - O(n) - cos finding in hashmap takes O(1) -
+    // constant time
+    public static void longestConsecutiveElementsOptimised(int[] arr) {
+        // first traversal - fill hashmap with boolean values
+        HashMap<Integer, Boolean> hash = new HashMap<>();
+        for (int val : arr)
+            hash.put(val, true);
+        // second traversal - make false if there exist an element arr[i]-1 for arr[i]
+        // in hashmap - cos in that case arr[i] can't be starting ele of any sequence
+        for (int val : arr)
+            if (hash.containsKey(val - 1))
+                hash.put(val, false);
+        // third traversal - only work for those who are true in hashmap
+        int maxStartPoint = 0;
+        int maxLen = 0;
+        for (int val : arr) {
+            if (hash.get(val) == true) {
+                int tempLen = 1;
+                int tempStartPoint = val;
+                while (hash.containsKey(tempStartPoint + tempLen))
+                    tempLen++;
+                if (tempLen > maxLen) {
+                    maxStartPoint = tempStartPoint;
+                    maxLen = tempLen;
+                }
+            }
+        }
+        for (int i = 0; i < maxLen; i++)
+            System.out.print(maxStartPoint + i + " ");
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -40,6 +72,7 @@ public class longestConsecutiveSequenceOfElements {
         String[] values = br.readLine().split(" ");
         for (int i = 0; i < n; i++)
             arr[i] = Integer.parseInt(values[i]);
-        longestConsecutiveElementsBrute(arr);
+        // longestConsecutiveElementsBrute(arr);
+        longestConsecutiveElementsOptimised(arr);
     }
 }
