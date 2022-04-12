ecb743a83cc996f212476571ed5066f3f92af3d4
Sushant2 <er.sushant.2@gmail.com>
2022-04-12 19:44:13 +0530

    k largest element brute force

--- Heap/kLargestElement.java
+++ Heap/kLargestElement.java
@@ -11,6 +11,18 @@ public class kLargestElement {
             System.out.println(arr[i]);
     }
 
+    // ! Optimised Approach - use MaxHeap - O(nlogn + klogn) //insertion & k
+    // elements remove
+    // store every element in max heap & pop first k elements
+    public static void kLargestMaxHeap(int[] arr, int k) {
+        // maxheap priority queue
+        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        for (int val : arr)
+            pq.add(val);
+        while (k-- > 0)
+            System.out.print(pq.remove() + " ");
+    }
+
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = Integer.parseInt(br.readLine());
@@ -19,7 +31,7 @@ public class kLargestElement {
         for (int i = 0; i < n; i++)
             arr[i] = Integer.parseInt(values[i]);
         int k = Integer.parseInt(br.readLine());
-        kLargest(arr, k);
-        // kLargestMaxHeap(arr, k);
+        // kLargest(arr, k);
+        kLargestMaxHeap(arr, k);
     }
 }
