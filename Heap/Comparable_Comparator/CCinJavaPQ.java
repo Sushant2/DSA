a81a34a123d7981a3a4411e515486f58fec77b2e
Sushant2 <er.sushant.2@gmail.com>
2022-04-13 19:32:41 +0530

    comparable in Java's PQ

--- Heap/Comparable_Comparator/CCinJavaPQ.java
+++ Heap/Comparable_Comparator/CCinJavaPQ.java
@@ -5,12 +5,44 @@ import java.io.*;
 
 public class CCinJavaPQ {
 
+    // class student
+    // class student implements comparable interface(which have compareTo method)
+    static class Student implements Comparable<Student> {
+        int rno;
+        int ht;
+        int wt;
+
+        Student(int rno, int ht, int wt) {
+            this.rno = rno;
+            this.ht = ht;
+            this.wt = wt;
+        }
+
+        // sorted acc to roll number - in ascending order
+        // public int compareTo(Student other) {
+        // return this.rno - other.rno;
+        // }
+
+        // sorted acc to roll number - in descending order
+        public int compareTo(Student other) {
+            return other.rno - this.rno;
+        }
+
+        // for printing student objects make tostring function
+        public String toString() {
+            return "rno= " + this.rno + ", ht= " + this.ht + ", wt= " + this.wt;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
-        PriorityQueue<Integer> pq = new PriorityQueue<>();
-        int[] arr = { 10, 2, 17, 3, 18, 9, 22 };
-        for (int val : arr)
-            pq.add(val);
+        PriorityQueue<Student> pq = new PriorityQueue<>();
+        pq.add(new Student(1, 180, 70));
+        pq.add(new Student(3, 157, 60));
+        pq.add(new Student(5, 170, 90));
+        pq.add(new Student(12, 145, 88));
+        pq.add(new Student(16, 134, 75));
+
         while (!pq.isEmpty())
-            System.out.print(pq.remove() + " ");
+            System.out.println(pq.remove() + " ");
     }
 }
