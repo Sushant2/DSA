c4455d9e9e13381075d67cf579ab95e2c23b0e8b
Sushant2 <er.sushant.2@gmail.com>
2022-04-15 16:01:59 +0530

    fibonacci optimised using memoization

--- Dynamic_Programming/fibonacci.java
+++ Dynamic_Programming/fibonacci.java
@@ -15,6 +15,27 @@ public class fibonacci {
         return fibn;
     }
 
+    // ! USING MEMOIZATION method - answers memorise krenge
+    // like a question bank array banaenege jo repeat problems h unka anser store
+    // kralenge & more no.of faltu calls se bachenge
+
+    public int fibMemoized(int n, int[] qb) {
+        if (n == 0 || n == 1)
+            return n;
+
+        // preorder m check krlenge already que ka answer exist to nhi krta na
+        if (qb[n] != 0)
+            return qb[n];
+
+        int fibnm1 = fibMemoized(n - 1, qb);
+        int fibnm2 = fibMemoized(n - 2, qb);
+        int fibn = fibnm1 + fibnm2;
+
+        // post order store krlenge ques n answer
+        qb[n] = fibn;
+        return fibn;
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
