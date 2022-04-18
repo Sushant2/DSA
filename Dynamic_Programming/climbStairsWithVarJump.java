8d46c8279075b626a55f1eb980408892f736a263
Sushant2 <er.sushant.2@gmail.com>
2022-04-18 16:21:38 +0530

    climb stairs with jump memoized

--- Dynamic_Programming/climbStairsWithVarJump.java
+++ Dynamic_Programming/climbStairsWithVarJump.java
@@ -9,6 +9,7 @@ public class climbStairsWithVarJump {
             return 0;
         if (i == n)
             return 1;
+        System.out.println("Hello " + i);
         int paths = 0;
         for (int j = 1; j <= arr[i]; j++) {
             paths += recursive(i + j, n, arr);
@@ -16,8 +17,26 @@ public class climbStairsWithVarJump {
         return paths;
     }
 
+    public static int memoization(int i, int n, int[] arr, int[] qb) {
+        // base case
+        if (i > n)
+            return 0;
+        if (i == n)
+            return 1;
+        if (qb[i] != 0)
+            return qb[i];
+        System.out.println("Hello " + i);
+
+        int paths = 0;
+        for (int j = 1; j <= arr[i]; j++)
+            paths += memoization(i + j, n, arr, qb);
+        qb[i] = paths;
+        return paths;
+    }
+
     public static int climbStairsJumps(int n, int[] arr) {
-        return recursive(0, n, arr);
+        // return recursive(0, n, arr);
+        return memoization(0, n, arr, new int[n + 1]);
     }
 
     public static void main(String[] args) throws Exception {
