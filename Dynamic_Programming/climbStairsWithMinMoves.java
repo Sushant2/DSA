b40fe0e388a0a9705e06013e4221a651672df19c
Sushant2 <er.sushant.2@gmail.com>
2022-04-19 00:07:26 +0530

    climb stairs with minimum moves memoized

--- Dynamic_Programming/climbStairsWithMinMoves.java
+++ Dynamic_Programming/climbStairsWithMinMoves.java
@@ -13,13 +13,35 @@ public class climbStairsWithMinMoves {
             int ans = recursive(i + j, n, arr);
             // jab koi na koi move aa rha ho tab hi min check krenge
             if (ans != Integer.MAX_VALUE)
-                min = Math.min(min, ans + 1); 
+                min = Math.min(min, ans + 1);
         }
         return min;
     }
 
+    public static int memoized(int i, int n, int[] arr, int[] qb) {
+        if (i > n)
+            return Integer.MAX_VALUE;
+        if (i == n)
+            return 0;
+
+        if (qb[i] != -1)
+            return qb[i];
+        int min = Integer.MAX_VALUE;
+        for (int j = 1; j <= arr[i]; j++) {
+            int ans = recursive(i + j, n, arr);
+            // jab koi na koi move aa rha ho tab hi min check krenge
+            if (ans != Integer.MAX_VALUE)
+                min = Math.min(min, ans + 1);
+        }
+        qb[i] = min;
+        return min;
+    }
+
     public static int climbStairs(int n, int[] arr) {
-        return recursive(0, n, arr);
+        // return recursive(0, n, arr);
+        int[] qb = new int[n + 1];
+        Arrays.fill(qb, -1);
+        return memoized(0, n, arr, qb);
     }
 
     public static void main(String[] args) throws Exception {
