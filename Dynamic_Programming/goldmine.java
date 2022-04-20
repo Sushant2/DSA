7112a71469e8a62c1ec98aa2e17aff175c2f231c
Sushant2 <er.sushant.2@gmail.com>
2022-04-20 21:29:00 +0530

    goldmine/path with max gold using tabulation

--- Dynamic_Programming/goldmine.java
+++ Dynamic_Programming/goldmine.java
@@ -45,18 +45,58 @@ public class goldmine {
         return qb[sr][sc];
     }
 
-    public static int maxGold(int[][] arr) {
-        int maxGold = Integer.MIN_VALUE;
-        int[][] qb = new int[arr.length][arr[0].length];
-        for (int i = 0; i < arr.length; i++)
-            for (int j = 0; j < arr[0].length; j++)
-                qb[i][j] = -1;
-        for (int i = 0; i < arr.length; i++) {
-            // int tempMax = recursive(arr, i, 0);
-            int tempMax = memoized(arr, i, 0, qb);
-            maxGold = Math.max(tempMax, maxGold);
+    public static int tabulation(int[][] arr) {
+        // step 1. create storage & assing meaning
+        int[][] dp = new int[arr.length][arr[0].length];
+        // meaning to cell - dp[i][j] = means agar yaha se agar yaha se gold dig krna
+        // start krte to kitna max collect krte
+        // step 2. direction of problem
+        // choti problem - last column, badi problem - first column, many ways to get
+        // gold
+        // column wise traverse from right to left
+        // step 3. travel & solve
+        for (int j = arr[0].length - 1; j >= 0; j--) {
+            for (int i = arr.length - 1; i >= 0; i--) {
+                // last column
+                if (j == arr[0].length - 1)
+                    dp[i][j] = arr[i][j];
+                // edge case - if only one row is given - we can dig only in it's right
+                else if (arr.length == 1)
+                    dp[i][j] = arr[i][j] + dp[i][j + 1];
+                // first row
+                else if (i == 0)
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i + 1][j + 1]);
+                // last row
+                else if (i == arr.length - 1)
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], dp[i - 1][j + 1]);
+                // rest 2d dp arraay
+                else
+                    dp[i][j] = arr[i][j] + Math.max(dp[i][j + 1], Math.max(dp[i + 1][j + 1], dp[i - 1][j + 1]));
+            }
         }
-        return maxGold;
+        // ans is maximum from first column
+
+        int max = dp[0][0];
+        for (int i = 1; i < dp.length; i++)
+            if (dp[i][0] > max)
+                max = dp[i][0];
+        return max;
+    }
+
+    public static int maxGold(int[][] arr) {
+        // int maxGold = Integer.MIN_VALUE;
+        // int[][] qb = new int[arr.length][arr[0].length];
+        // for (int i = 0; i < arr.length; i++)
+        // for (int j = 0; j < arr[0].length; j++)
+        // qb[i][j] = -1;
+        // for (int i = 0; i < arr.length; i++) {
+        // int tempMax = recursive(arr, i, 0);
+        // int tempMax = memoized(arr, i, 0, qb);
+        // maxGold = Math.max(tempMax, maxGold);
+        // }
+        // return maxGold;
+
+        return tabulation(arr);
     }
 
     public static void main(String[] args) throws Exception {
