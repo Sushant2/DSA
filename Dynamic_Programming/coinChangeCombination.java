c3499ba7fd984fe020b5760f84da9c624925b9e7
Sushant2 <er.sushant.2@gmail.com>
2022-04-22 19:57:37 +0530

    coin change combination using tabulation

--- Dynamic_Programming/coinChangeCombination.java
+++ Dynamic_Programming/coinChangeCombination.java
@@ -39,12 +39,38 @@ public class coinChangeCombination {
             return qb[n][amt] = memoized(arr, n - 1, amt, qb);
     }
 
+    public static int tabulation(int[] arr, int n, int amt) {
+        // step 1. create storage & assign meaning to cells
+        int[] dp = new int[amt + 1];
+        // meaning of cell - e.g., for dp[j] = j ko pay krne k kitne combinations hai
+        // using these coins
+        // ! 0(zero) ko pay krne ka hamesha ek rasta hoga - ki kuch pay mat kro
+        dp[0] = 1;
+        // step 2. direction of problem - choti problem - dp[0], badi problem dp[n]
+        // step 2. travel & solve
+        // e.g. = arr = {2,3,5} , amt = 7
+        // ! outer loop (coins wala)
+        // hme har coins ka effect dalna hai har amount k lie - pahle sare dp table m, 2
+        // ka loop lagaenge then 3, & then 5 & so on... & isse we're ensuring that
+        // combinations bani rhe, pahle sare 2, then 3, then 5
+        // ! we'll start inner loop from arr[i], cos usse pahle hum kuch pay hi nhi
+        // krpayemge
+
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = arr[i]; j < dp.length; j++) {
+                dp[j] += dp[j - arr[i]];
+            }
+        }
+        return dp[amt];
+    }
+
     public static int coinChange(int[] arr, int n, int amt) {
         // return recursive(arr, n, amt);
         int[][] qb = new int[n + 1][amt + 1];
         for (int[] x : qb)
             Arrays.fill(x, -1);
-        return memoized(arr, n, amt, qb);
+        // return memoized(arr, n, amt, qb);
+        return tabulation(arr, n, amt);
     }
 
     public static void main(String[] args) throws Exception {
