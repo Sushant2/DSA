14f9e32f862e4e3b37b41d05b0da92199922ffe4
Sushant2 <er.sushant.2@gmail.com>
2022-04-23 02:37:01 +0530

    coin change permutation

--- Dynamic_Programming/coinChangePermutation.java
+++ Dynamic_Programming/coinChangePermutation.java
@@ -4,8 +4,8 @@ import java.util.*;
 public class coinChangePermutation {
 
     // find count of permutaions of given amount
-    // ! TIME COMPLEXITY - O(n^T) = n is no.of coins & t is target - exponential
-    // time compl
+    // ! TIME COMPLEXITY - O(n^T)
+    // n is no.of coins & t is target - exponential time compl
     public static int recursive(int[] arr, int amt) {
         // +base case
         if (amt == 0)
@@ -23,8 +23,27 @@ public class coinChangePermutation {
 
     }
 
+    public static int memoized(int[] arr, int amt, int[] qb) {
+        // base condition
+        /// qb[i] will denote the number of combinations possible with amount = i.
+        if (amt == 0)
+            return qb[amt] = 1;
+        // if answer to that problem is already present in qb, return
+        if (qb[amt] != -1)
+            return qb[amt];
+        int res = 0;
+        for (int coin : arr) {
+            if (coin <= amt)
+                res += memoized(arr, amt - coin, qb);
+        }
+        return qb[amt] = res;
+    }
+
     public static int coinChangePerm(int[] arr, int n, int amt) {
-        return recursive(arr, amt);
+        // return recursive(arr, amt);
+        int[] qb = new int[amt + 1];
+        Arrays.fill(qb, -1);
+        return memoized(arr, amt, qb);
     }
 
     public static void main(String[] args) throws Exception {
