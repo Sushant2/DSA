4a7110fe767076c0f2f611835ca246a6c6579928
Sushant2 <er.sushant.2@gmail.com>
2022-04-24 16:26:13 +0530

    01 knapsack memoized

--- Dynamic_Programming/zeroOneKnapSack.java
+++ Dynamic_Programming/zeroOneKnapSack.java
@@ -16,8 +16,26 @@ public class zeroOneKnapSack {
             return recursive(wt, val, W, n - 1);
     }
 
+    public static int memoized(int[] wt, int[] val, int W, int n, int[][] qb) {
+        // base condition
+        if (W == 0 || n == 0)
+            return 0;
+        if (qb[n][W] != 0)
+            return qb[n][W];
+
+        if (wt[n - 1] <= W) {
+            qb[n][W] = Math.max(val[n - 1] + memoized(wt, val, W - wt[n - 1], n - 1, qb),
+                    memoized(wt, val, W, n - 1, qb));
+            return qb[n][W];
+        } else {
+            return qb[n][W] = memoized(wt, val, W, n - 1, qb);
+        }
+    }
+
     public static int _01knapSack(int[] wt, int[] val, int W, int n) {
-        return recursive(wt, val, W, n);
+        // return recursive(wt, val, W, n);
+        int[][] qb = new int[n + 1][W + 1];
+        return memoized(wt, val, W, n, qb);
     }
 
     public static void main(String[] args) throws Exception {
