0285aeb7e700a8c8b91eb6f3b678b93b6f95e0df
Sushant2 <er.sushant.2@gmail.com>
2022-04-25 01:33:25 +0530

    unbounded knapsack using tabulation

--- Dynamic_Programming/unboundedKnapSack.java
+++ Dynamic_Programming/unboundedKnapSack.java
@@ -27,12 +27,40 @@ public class unboundedKnapSack {
         }
     }
 
+    public static int tabulation(int[] wt, int[] val, int W, int n) {
+        // step1: create storage & assign meaning to cell
+        int[] dp = new int[W + 1];
+        dp[0] = 0; // for weight 0, no profit = 0
+        // meaning to cell
+        // dp[i] will store what is the maxm profit for ith weight for these items
+        // step 2 : direction of problem
+        // choti prblm - dp[0], badi prblm - dp[W]
+        // step 3 : travel & solve
+        // outer loop - each weight
+        // inner loop - each item
+        for (int cap = 1; cap <= W; cap++) {
+            int max = 0;
+            for (int i = 0; i < n; i++) {
+                if (wt[i] <= cap) {
+                    int remC = cap - wt[i];
+                    int remV = dp[remC];
+                    int totalV = remV + val[i];
+                    if (totalV > max)
+                        max = totalV;
+                }
+            }
+            dp[cap] = max;
+        }
+        return dp[W];
+    }
+
     public static int unboundedKnap(int[] wt, int[] val, int W, int n) {
         // return recursive(wt, val, W, n);
         int[][] qb = new int[n + 1][W + 1];
         for (int[] x : qb)
             Arrays.fill(x, -1);
-        return memoized(wt, val, W, n, qb);
+        // return memoized(wt, val, W, n, qb);
+        return tabulation(wt, val, W, n);
     }
 
     public static void main(String[] args) throws Exception {
