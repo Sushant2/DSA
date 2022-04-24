c32c3c18aba7623e1ad71ea1d6240f66bb235929
Sushant2 <er.sushant.2@gmail.com>
2022-04-24 23:08:10 +0530

    unbounded knapsack using memoization

--- Dynamic_Programming/unboundedKnapSack.java
+++ Dynamic_Programming/unboundedKnapSack.java
@@ -13,8 +13,26 @@ public class unboundedKnapSack {
             return recursive(wt, val, W, n - 1);
     }
 
+    public static int memoized(int[] wt, int[] val, int W, int n, int[][] qb) {
+        if (n == 0 || W == 0)
+            return qb[n][W] = 0;
+        if (qb[n][W] != -1)
+            return qb[n][W];
+
+        if (wt[n - 1] <= W) {
+            return qb[n][W] = Math.max(val[n - 1] + memoized(wt, val, W - wt[n - 1], n, qb),
+                    memoized(wt, val, W, n - 1, qb));
+        } else {
+            return qb[n][W] = memoized(wt, val, W, n - 1, qb);
+        }
+    }
+
     public static int unboundedKnap(int[] wt, int[] val, int W, int n) {
-        return recursive(wt, val, W, n);
+        // return recursive(wt, val, W, n);
+        int[][] qb = new int[n + 1][W + 1];
+        for (int[] x : qb)
+            Arrays.fill(x, -1);
+        return memoized(wt, val, W, n, qb);
     }
 
     public static void main(String[] args) throws Exception {
