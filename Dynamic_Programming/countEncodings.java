14df220e054d8b310fb12f3f951552e41b4e815c
Sushant2 <er.sushant.2@gmail.com>
2022-04-26 21:03:42 +0530

    count encodings/decode ways using tabulation

--- Dynamic_Programming/countEncodings.java
+++ Dynamic_Programming/countEncodings.java
@@ -36,6 +36,39 @@ public class countEncodings {
         return qb[n] = sum;
     }
 
+    public static int tabulation(char[] str, int n) {
+        // step1: create storage & assign meaning to cells
+        int[] dp = new int[n];
+        dp[0] = 1;// 1 length ke lie 1 hi encoding hogi
+        // meaning of cell : dp[i] means ith string tak ke kitne encodings hai
+        // step2 : direction of problem
+        // small : dp[0], large: dp[n]
+        // step 3 : travel & solve
+        for (int i = 1; i < dp.length; i++) {
+            // case 1: if i-1 & i both are zeroes
+            if (str[i - 1] == '0' && str[i] == '0')
+                dp[i] = 0;
+            // case 2: if i-1 is zero & i is non zero
+            else if (str[i - 1] == '0' && str[i] != '0')
+                dp[i] = dp[i - 1];
+            // case 3: if i-1 is nonzero & i is zero
+            else if (str[i - 1] != '0' && str[i] == '0') {
+                if (str[i - 1] == '1' || str[i - 1] == '2')
+                    dp[i] = (i >= 2 ? dp[i - 2] : 1);
+                else
+                    dp[i] = 0;
+            }
+            // case 4: if i-1 & i both are nonzeros
+            else {
+                if (str[i - 1] == '1' || str[i - 1] == '2' && str[i] <= '6')
+                    dp[i] = dp[i - 1] + (i >= 2 ? dp[i - 2] : 1);
+                else
+                    dp[i] = dp[i - 1];
+            }
+        }
+        return dp[n - 1];
+    }
+
     public static int decodeWays(char[] str, int n) {
         if (n == 0 || (n == 1 && str[0] == '0'))
             return 0;
@@ -44,7 +77,8 @@ public class countEncodings {
         Arrays.fill(qb, -1);
         qb[0] = 0;
         qb[1] = 1;
-        return memoized(str, n, qb);
+        // return memoized(str, n, qb);
+        return tabulation(str, n);
     }
 
     public static void main(String[] args) throws Exception {
