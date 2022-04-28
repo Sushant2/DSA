afeef5e56e54b533e6a6855ac696edf0c597def4
Sushant2 <er.sushant.2@gmail.com>
2022-04-28 23:37:34 +0530

    maximum sum non adjacent elements using tabulation withextra space

--- Dynamic_Programming/maximumSumNonAdjacentElements.java
+++ Dynamic_Programming/maximumSumNonAdjacentElements.java
@@ -29,6 +29,7 @@ public class maximumSumNonAdjacentElements {
         return Math.max(incl, excl);
     }
 
+    // time comp : O(n), space comp : O(n)
     public static int memoized(int[] arr, int n, int[] qb) {
         if (n < 0)
             return 0;
@@ -42,12 +43,33 @@ public class maximumSumNonAdjacentElements {
         return result;
     }
 
+    public static int tabulation(int[] arr, int n) {
+        // step 1 : create storage & assign meaning to cells
+        // meaning : dp[0][i] stores maximum subsequencessum till ith index with arr[i]
+        // included
+        // where as, dp[1][i] stores maximum subsequences till ith index with arr[i]
+        // excluded
+        int[][] dp = new int[2][n];
+        // initialise
+        dp[0][0] = arr[0]; // incl
+        dp[1][0] = 0; // excl
+
+        // step2 :direction of problem - small : dp[0][0], large dp[1][n]
+        //step3 : travel & solve
+        for (int i = 1; i < n; i++) {
+            dp[0][i] = dp[1][i - 1] + arr[i];
+            dp[1][i] = Math.max(dp[0][i - 1], dp[1][i - 1]);
+        }
+        return Math.max(dp[0][n - 1], dp[1][n - 1]);
+    }
+
     public static int maximumSumNonAdjacentELe(int[] arr, int n) {
         // return recursive(arr, 0, n, Integer.MIN_VALUE);
         // return recursiveSimpleCode(arr, n - 1);
         int[] qb = new int[n + 1];
         Arrays.fill(qb, -1);
-        return memoized(arr, n - 1, qb);
+        // return memoized(arr, n - 1, qb);
+        return tabulation(arr, n);
     }
 
     public static void main(String[] args) throws Exception {
