e7cef4e88d6f09fbd04437e21a1fd6a7e13fbee9
Sushant2 <er.sushant.2@gmail.com>
2022-05-01 20:09:25 +0530

    friends pairing using memoization

--- Dynamic_Programming/friendsPairing.java
+++ Dynamic_Programming/friendsPairing.java
@@ -15,8 +15,28 @@ public class friendsPairing {
         return solo + pair;
     }
 
+    // time compl : O(n), space comp : O(n)
+    public static int memoized(int n, int[] qb) {
+        if (n <= 1)
+            return 1;
+        if (qb[n] != -1)
+            return qb[n];
+        int solo = memoized(n - 1, qb);
+        int pair = memoized(n - 2, qb) * (n - 1);
+        int ways = solo + pair;
+        qb[n] = ways;
+        return ways;
+    }
+
+
+
     public static int findPairingWays(int n) {
-        return recursive(n);
+        // return recursive(n);
+        int[] qb = new int[n + 1];
+        Arrays.fill(qb, -1);
+        qb[0] = qb[1] = 1;
+        // return memoized(n, qb);
+        return tabulation(n);
     }
 
     public static void main(String[] args) throws Exception {
