b6651d38d050feb52ce35896e9298df4e865a6d8
Sushant2 <er.sushant.2@gmail.com>
2022-05-04 23:47:36 +0530

    buy and sell stock with atmost 2 transaction using memoization

--- Dynamic_Programming/Buy And Sell/buynSellStockWithAtMost2Trans.java
+++ Dynamic_Programming/Buy And Sell/buynSellStockWithAtMost2Trans.java
@@ -16,9 +16,35 @@ public class buynSellStockWithAtMost2Trans {
                     0 + recursive(idx + 1, 0, prices, cap));
     }
 
+    public static int memoization(int idx, int buy, int[] prices, int cap, int[][][] qb) {
+        // base case
+        if (idx == prices.length || cap == 0)
+            return 0;
+
+        if (qb[idx][buy][cap] != -1)
+            return qb[idx][buy][cap];
+
+        // if buy = true
+        if (buy == 1)
+            return qb[idx][buy][cap] = Math.max(-prices[idx] + memoization(idx + 1, 0, prices, cap, qb),
+                    0 + memoization(idx + 1, 1, prices, cap, qb));
+        else
+            return qb[idx][buy][cap] = Math.max(prices[idx] + memoization(idx + 1, 1, prices, cap - 1, qb),
+                    0 + memoization(idx + 1, 0, prices, cap, qb));
+    }
+
     public static int findProfit(int[] prices) {
         // idx, buytrue, prices, cap
-        return recursive(0, 1, prices, 2);
+        // return recursive(0, 1, prices, 2);
+        // we can see 3 parameters are changing-idx,buy,cap
+        // so creating 3d dp array
+        // 2 states for buy, & 3 states of cap, 2,1, 0(means no transaction)
+        int[][][] qb = new int[prices.length][2][3];
+        for (int[][] x : qb) {
+            for (int[] y : x)
+                Arrays.fill(y, -1);
+        }
+        return memoization(0, 1, prices, 2, qb);
     }
 
     public static void main(String[] args) throws Exception {
