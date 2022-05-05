5d9b3c90bf6b29b7ee46801a48c9d3c7efb11ef4
Sushant2 <er.sushant.2@gmail.com>
2022-05-05 23:20:34 +0530

    buy and sell stock with atmost k transaction using memoization

--- Dynamic_Programming/Buy And Sell/buynSellStocksWithAtMostKTrans.java
+++ Dynamic_Programming/Buy And Sell/buynSellStocksWithAtMostKTrans.java
@@ -2,6 +2,7 @@ import java.io.*;
 import java.util.*;
 
 public class buynSellStocksWithAtMostKTrans {
+    // time comp : O(2^n), space compl : O(n)
     public static int recursive(int idx, int buy, int k, int[] prices) {
         // base case
         if (idx == prices.length || k == 0)
@@ -13,8 +14,33 @@ public class buynSellStocksWithAtMostKTrans {
         }
     }
 
+    // time comp: O(n*2*K), space comp: O(n*2*k)
+    public static int memoized(int idx, int buy, int k, int[] prices, int[][][] qb) {
+        // base case
+        if (idx == prices.length || k == 0)
+            return 0;
+        if (qb[idx][buy][k] != -1)
+            return qb[idx][buy][k];
+        if (buy == 1) {
+            return qb[idx][buy][k] = Math.max(-prices[idx] + memoized(idx + 1, 0, k, prices, qb),
+                    0 + memoized(idx + 1, 1, k, prices, qb));
+        } else {
+            return qb[idx][buy][k] = Math.max(prices[idx] + memoized(idx + 1, 1, k - 1, prices, qb),
+                    0 + memoized(idx + 1, 0, k, prices, qb));
+        }
+    }
+
+    // time comp: O(n*2*K), space comp: O(n*2*k)
+
     public static int findProfit(int[] prices, int k) {
-        return recursive(0, 1, k, prices);
+        // return recursive(0, 1, k, prices);
+        int[][][] qb = new int[prices.length][2][k + 1];
+        for (int[][] x : qb) {
+            for (int[] y : x) {
+                Arrays.fill(y, -1);
+            }
+        }
+        return memoized(0, 1, k, prices, qb);
     }
 
     public static void main(String[] args) throws Exception {
