59ef366870c4ef7402cd870556e3943f977f1219
Sushant2 <er.sushant.2@gmail.com>
2022-05-11 00:15:10 +0530

    weighted graph representation using adjacency matrix

--- Graph/adjacencyMatrix.java
+++ Graph/adjacencyMatrix.java
@@ -2,20 +2,34 @@ import java.io.*;
 import java.util.*;
 
 public class adjacencyMatrix {
+
+    public static class Pair {
+        int val, weight;
+
+        Pair(int val, int weight) {
+            this.val = val;
+            this.weight = weight;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
         int m = scn.nextInt();
-        int[][] adjMat = new int[n + 1][n + 1];
+        Pair[][] adjMat = new Pair[n + 1][n + 1];
         for (int i = 0; i < m; i++) {
             int u = scn.nextInt();
             int v = scn.nextInt();
-            adjMat[u][v] = 1;
-            adjMat[v][u] = 1;
+            int weight = scn.nextInt();
+            adjMat[u][v] = new Pair(1, weight);
+            adjMat[v][u] = new Pair(1, weight);
         }
         for (int i = 0; i < adjMat.length; i++) {
             for (int j = 0; j < adjMat[0].length; j++)
-                System.out.print(adjMat[i][j] + " ");
+                if (adjMat[i][j] != null)
+                    System.out.print(adjMat[i][j].weight + " ");
+                else
+                    System.out.print("0 ");
             System.out.println();
         }
     }
