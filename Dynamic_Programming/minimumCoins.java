7d96a6cdc9f1cd2cdb0ba1776cf97ff96e74f27b
Sushant2 <er.sushant.2@gmail.com>
2022-05-10 18:19:00 +0530

    minimum coins using memoization

--- Dynamic_Programming/minimumCoins.java
+++ Dynamic_Programming/minimumCoins.java
@@ -3,6 +3,7 @@ import java.io.*;
 
 public class minimumCoins {
 
+    // time compl : >>O(2^n) or exponential, space compl : >>O(n) or O(target)
     public static int recursive(int[] arr, int ind, int target) {
         // base condition
         if (ind == 0) {
@@ -21,8 +22,33 @@ public class minimumCoins {
         return Math.min(notTake, take);
     }
 
+    // time compl : >>O(2^n) or exponential, space compl : >>O(n) or O(target)
+    public static int memoization(int[] arr, int ind, int target, int[][] qb) {
+        // base condition
+        if (ind == 0) {
+            if (target % arr[0] == 0)
+                return target / arr[0];
+            else
+                return (int) Math.pow(10, 9);
+        }
+        if (qb[ind][target] != -1)
+            return qb[ind][target];
+
+        // if not taking then count = 0, & if taking that coin, adding 1 to the count
+        int notTake = 0 + recursive(arr, ind - 1, target);
+        int take = (int) Math.pow(10, 9);
+        if (arr[ind] <= target)
+            take = 1 + recursive(arr, ind, target - arr[ind]);
+
+        return qb[ind][target] = Math.min(notTake, take);
+    }
+
     public static int findMinCoins(int[] arr, int n, int target) {
-        return recursive(arr, n - 1, target);
+        // return recursive(arr, n - 1, target);
+        int[][] qb = new int[n][target + 1];
+        for (int[] x : qb)
+            Arrays.fill(x, -1);
+        return memoization(arr, n - 1, target, qb);
     }
 
     public static void main(String[] args) throws Exception {
