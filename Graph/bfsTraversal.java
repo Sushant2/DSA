afff874b536b01d2ac3a9faa037a8af6ff30ce22
Sushant2 <er.sushant.2@gmail.com>
2022-05-11 21:14:51 +0530

    bfs traversal for disconnected graph

--- Graph/bfsTraversal.java
+++ Graph/bfsTraversal.java
@@ -31,21 +31,48 @@ public class bfsTraversal {
         return bfs;
     }
 
+    public static ArrayList<Integer> getBFS2(ArrayList<ArrayList<Integer>> adj, int V) {
+        // BFS Traversal
+        // if graph is having multiple components
+        ArrayList<Integer> bfs = new ArrayList<>();
+        boolean visited[] = new boolean[V + 1];
+        for (int i = 1; i <= V; i++) {
+            // iterating every node
+            if (visited[i] == false) {
+                Queue<Integer> q = new ArrayDeque<>();
+                q.add(i);
+                visited[i] = true;
+                while (!q.isEmpty()) {
+                    Integer node = q.remove();
+                    bfs.add(node);
+
+                    for (Integer x : adj.get(node)) {
+                        if (visited[x] == false) {
+                            q.add(x);
+                            visited[x] = true;
+                        }
+                    }
+                }
+            }
+        }
+        return bfs;
+    }
+
     public static void main(String[] args) throws Exception {
-        int V = 4;
+        int V = 7;
         ArrayList<ArrayList<Integer>> adj = new ArrayList<ArrayList<Integer>>(V);
 
-        for (int i = 0; i < V; i++)
+        for (int i = 0; i < V + 1; i++)
             adj.add(new ArrayList<Integer>());
 
-        addEdge(adj, 0, 1);
-        addEdge(adj, 0, 2);
         addEdge(adj, 1, 2);
-        addEdge(adj, 2, 0);
         addEdge(adj, 2, 3);
-        addEdge(adj, 3, 3);
+        addEdge(adj, 2, 7);
+        addEdge(adj, 3, 5);
+        addEdge(adj, 5, 7);
+        addEdge(adj, 4, 6);
 
-        ArrayList<Integer> bfsTraversal = getBFS(adj, 2, V);
+        ArrayList<Integer> bfsTraversal = getBFS2(adj, V);
         System.out.println(bfsTraversal);
 
     }
