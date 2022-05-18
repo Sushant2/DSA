67671da3851d0296e38aefd4980282bb9743bdd8
Sushant2 <er.sushant.2@gmail.com>
2022-05-18 22:05:51 +0530

    dijkstra's algorithm initials

--- Graph/dijkstraAlgo.java
+++ Graph/dijkstraAlgo.java
@@ -3,6 +3,7 @@ import java.util.*;
 
 public class dijkstraAlgo {
     public static void main(String[] args) {
-        
+        // it's same as shortest path in undirected graph, but there we have unit
+        // weights & here we've different weights!
     }
 }
