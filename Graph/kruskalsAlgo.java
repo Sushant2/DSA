66b962c0797fcd1ca88d05f8fe4230b136546143
Sushant2 <er.sushant.2@gmail.com>
2022-05-22 00:19:23 +0530

    kruskal's algorithm for finding MST uses DSU

--- Graph/kruskalsAlgo.java
+++ Graph/kruskalsAlgo.java
@@ -19,6 +19,44 @@ public class kruskalsAlgo {
         }
     }
 
+    public static int findPar(int node, int[] parent) {
+        if (parent[node] == node)
+            return node;
+        return parent[node] = findPar(parent[node], parent);
+    }
+
+    public static void union(int u, int v, int[] parent, int[] rank) {
+        u = findPar(u, parent);
+        v = findPar(v, parent);
+        if (rank[u] < rank[v])
+            parent[u] = v;
+        else if (rank[u] > rank[v])
+            parent[v] = u;
+        else {
+            parent[v] = u;
+            rank[u]++;
+        }
+    }
+
+    public static int getMST(ArrayList<Node> adj, int N) {
+        Collections.sort(adj, new sortComparator());
+        int[] parent = new int[N];
+        int[] rank = new int[N];
+        for (int i = 0; i < N; i++)
+            parent[i] = i;
+        int costMST = 0;
+        ArrayList<Node> mst = new ArrayList<>();
+        // traversing every edge
+        for (Node it : adj) {
+            if (findPar(it.u, parent) != findPar(it.v, parent)) {
+                costMST += it.weight;
+                mst.add(it);
+                union(it.u, it.v, parent, rank);
+            }
+        }
+        return costMST;
+    }
+
     public static void main(String[] args) {
         int n = 5;
         // using a linear DS not adjacency list to store graph
@@ -29,6 +67,7 @@ public class kruskalsAlgo {
         adj.add(new Node(1, 2, 3));
         adj.add(new Node(1, 4, 5));
         adj.add(new Node(2, 4, 7));
-        int minCcst = getMST(adj, n);
+        int minCost = getMST(adj, n);
+        System.out.println(minCost);
     }
 }
