1dcda5d41f02f03627144334845b32e4c9501abe
Sushant2 <er.sushant.2@gmail.com>
2022-05-25 15:56:01 +0530

    diagonal Traversal of binary tree

--- zLevel 2/Trees/diagonalTraversalOfBTree.java
+++ zLevel 2/Trees/diagonalTraversalOfBTree.java
@@ -1,6 +1,7 @@
 //TODO - GFG - Diagonal Traversal of binary tree
 // [https://practice.geeksforgeeks.org/problems/diagonal-traversal-of-binary-tree/1]
 
+// accpeted in GFG
 class Tree {
     static int maxD = 0;
 
@@ -27,6 +28,9 @@ class Tree {
         getDiagonals(root, 0, map);
         for (int i = 0; i <= maxD; i++) {
             ArrayList<Integer> list = map.get(i);
+            // list equals null cos - jab root hi null ho, but hmne static m md =0 to
+            // banahidia, so, ek baar to for loop chalega & use khali arraylist milegi
+            // hashmap se
             if (list == null)
                 continue;
             for (Integer x : list)
@@ -37,7 +41,7 @@ class Tree {
 }
 
 // ! this code is BFS - level order traversal but doesn't work cos GFG one
-// requires in DFS only
+// requires in DFS only - not accpeted in GFG
 class Tree {
     public static class Pair {
         Node node;
@@ -76,6 +80,30 @@ class Tree {
             ArrayList<Pair> pairs = map.get(i);
             for (Pair x : pairs)
                 ans.add(x.node.data);
+        }
+        return ans;
+    }
+}
+
+// "TWEEK BFS" - approach - simple - accpeted in GFG
+// at any node - use node ko ans mein add kr rhe hai, & agar uski left hai to
+// queue mein put other wise right h to aage badke ans mein add
+
+class Tree {
+    public ArrayList<Integer> diagonal(Node root) {
+        ArrayList<Integer> ans = new ArrayList<>();
+        Queue<Node> q = new LinkedList<>();
+        q.add(root);
+        while (!q.isEmpty()) {
+            Node node = q.remove();
+
+            while (node != null) {
+                ans.add(node.data);
+                if (node.left != null) {
+                    q.add(node.left);
+                }
+                node = node.right;
+            }
         }
         return ans;
     }
